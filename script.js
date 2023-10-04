ac862dd033436c4f811ee2a82f6efd1ed7650503
jackie-hen <144840436+jackie-hen@users.noreply.github.com>
2023-10-04 17:57:12 +0400

    Add files via upload

---
+++ script.js
@@ -0,0 +1,26 @@
+let lightShape=document.getElementById("light")
+lightShape.addEventListener("click", () => {
+  alert("This color helps her blend to the sky!");
+})
+
+let darkShape=document.getElementById("dark")
+darkShape.addEventListener("click", () => {
+  alert("The tops of her wings are dark blue from eating so many berries!");
+})
+
+let greenShape=document.getElementById("green")
+greenShape.addEventListener("click", () => {
+  alert("The green is for when she's hiding in the forest!");
+})
+
+
+
+let blackShape=document.getElementById("black")
+blackShape.addEventListener("click", () => {
+  alert("Her dark body helps her sneak while she hunts!");
+})
+
+let medShape=document.getElementById("medium")
+medShape.addEventListener("click", () => {
+  alert("This color has no purpose but it's super pretty!");
+})
